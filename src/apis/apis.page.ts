2082ec5713b9fbd370abf06a64f66b1e6c5a5224
Phil Hawksworth <phil@deno.com>
2025-01-08 15:08:43 +0000

    api pages generation

---
+++ src/apis/apis.page.ts
@@ -0,0 +1,14 @@
+import docs  from "../_data/docs.json" with { type: "json" };
+
+const apis = docs.nodes[1].namespaceDef.elements;
+
+export default function* () {
+
+  for (const item of apis) {
+    yield {
+      layout: "layouts/api.vto",
+      url: `/apis/${item.kind}/${item.name}-${item.location.byteIndex}`,  
+      ...item,   
+    };
+  }
+}
